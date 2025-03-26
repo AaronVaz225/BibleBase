c946e9752af2dc4a32de71188f00303b3acabf71
Aaron Vazquez <aaronrvazquez01@gmail.com>
2025-03-26 14:42:26 -0500

    styled nav bar

--- client/bible-base/src/components/Cards/ProfileInfo.jsx
+++ client/bible-base/src/components/Cards/ProfileInfo.jsx
@@ -3,14 +3,14 @@ import { getInitials } from "../../../utils/helper";
 
 const ProfileInfo = ({ userInfo, onLogout }) => {
   return (
-    <div className="flex items-center gap-3">
-      <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100">
+    <div className="flex items-center gap-3 pb-2">
+      <div className="w-12 h-12 flex items-center justify-center rounded-full text-slate-100 font-medium bg-gradient-to-r from-violet-600 to-indigo-600">
         {getInitials(userInfo?.fullName)}
       </div>
       <div>
-        <p className="text-sm font-medium">{userInfo?.fullName}</p>
+        <p className="text-lg font-mono font-medium">{userInfo?.fullName}</p>
         <button
-          className="text-sm text-slate-700 underline cursor-pointer"
+          className="text-sm text-slate-700 underline cursor-pointer font-mono"
           onClick={onLogout}
         >
           Logout
--- client/bible-base/src/components/NavBar/Navbar.jsx
+++ client/bible-base/src/components/NavBar/Navbar.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import ProfileInfo from "../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import Logo from "../../../public/BibleBase.png";
 
 const Navbar = ({ userInfo }) => {
   const navigate = useNavigate();
@@ -13,8 +14,13 @@ const Navbar = ({ userInfo }) => {
   };
 
   return (
-    <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
-      <h2 className="text-xl font-medium text-black py-2">BibleBase</h2>
+    <div className="bg-[#d9dcd6] flex items-center justify-between pl-2 pr-5 py-2 drop-shadow">
+      <div className="flex  ">
+        <img src={Logo} className="pr-4 max-w-15 min-w-15" />
+        <h2 className="text-2xl font-bold text-[#260038] font-mono py-2">
+          BibleBase
+        </h2>
+      </div>
 
       <ProfileInfo userInfo={userInfo} onLogout={onLogout} />
     </div>
